f61f5a73abb88b0cdab3ec23b36e9b54ebea239a
ROW <axlrower@gmail.com>
2025-04-11 19:12:50 -0400

    creacion header

--- hito-pizzeria/src/App.jsx
+++ hito-pizzeria/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Header from "./components/Header/header";
 
 function App() {
-  
-
   return (
     <>
-      
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ hito-pizzeria/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css';
+
+export default function header() {
+  return (
+    <div className='food'>
+      <p className='pizza'>!Pizzeria Mamma Mia!</p>
+      <p className='title1'><strong>Tenemos las mejores pizzas que podrás encontrar</strong></p>
+    </div>
+  )
+}
