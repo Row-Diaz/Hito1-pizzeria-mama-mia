a1f75bfb3cc6fefdf195fd45bb25bc493c578bba
ROW <axlrower@gmail.com>
2025-04-12 02:20:09 -0400

    cards 1er version

--- hito-pizzeria/src/App.jsx
+++ hito-pizzeria/src/App.jsx
@@ -7,18 +7,25 @@ import CardPizza from "./components/Home/CardPizza";
 
 
 
+
 function App() {
 const imageURl = "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"
 const altText = "pizza napolitana";
   
   return (
     <>
+      <div>
       <Navbar />
       <Header />
       <CardPizza imageURl={imageURl} altText={altText} />
       <CardPizza name="Pizza Napolitana" />
       <CardPizza ingredients={["mozzarella,", "tomates,", "jamón,", "orégano"]} />
       <CardPizza price="Precio:$5.950" />
+      </div>
+
+      
+      
+      
     </>
   );
 }
