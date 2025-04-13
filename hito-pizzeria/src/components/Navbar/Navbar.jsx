0ba4dc161715b273ad9f584292e109e7bbfebc86
ROW <axlrower@gmail.com>
2025-04-13 13:01:52 -0400

    version final1

--- hito-pizzeria/src/components/Navbar/Navbar.jsx
+++ hito-pizzeria/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,9 @@
 import React from "react";
 import "./Navbar.css";
 
-// export default function Navbar() {
-
-//   const total = 25000;
-// const token = false;
-
-//   return (
-//     <div className="nav">
-//       <p>"Pizzeria Mamma Mia!"</p>
-
-//       <div className="btn-nav button">
-//       <button>🍕Home</button>
-//       <button>🙍‍♂️Profile</button>
-//       <button>🔒Logout</button>
-//       <button>🔐Login</button>
-//       <button>🔑 Register</button>
-//       </div>
-//       <div className="amount">
-//         <button>🛒Total </button>
-//       </div>
-
-//     </div>
-//   );
-// }
-
 const Navbar = () => {
-  const total = 25000;
-  const token = true;
+  const total = 2500;
+  const token = false;
 
   // Función para formatear el total
   const formatCurrency = (amount) => {
@@ -43,8 +19,8 @@ const Navbar = () => {
         <h1>Pizeria Mamma Mia!</h1>
       </div>
       <div className="navbarBtn">
-       <button>Home</button>
-       <button>Contacto</button>
+        <button>Home</button>
+        <button>Contacto</button>
       </div>
       <div className="navbar-cart">
         <button className="total-btn">🛒 Total: {formatCurrency(total)}</button>
