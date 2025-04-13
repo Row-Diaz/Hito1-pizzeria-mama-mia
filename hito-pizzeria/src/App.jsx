33f4636a577f0be861cf07aa7275d4e397c65337
ROW <axlrower@gmail.com>
2025-04-13 13:19:13 -0400

    ajustes

--- hito-pizzeria/src/App.jsx
+++ hito-pizzeria/src/App.jsx
@@ -2,25 +2,21 @@ import { useState } from "react";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import CardPizza from "./components/Cards/CardPizza";
-import FooterJs from "./components/Footer/FooterJs"
-
-
-
+import FooterJs from "./components/Footer/FooterJs";
 
 function App() {
-   const imageUrl =
-       "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c";
-   
-     const imageUrl1 =
-       "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab";
-   
-     const imageUrl2 =
-       "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3";
+  const imageUrl =
+    "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c";
+
+  const imageUrl1 =
+    "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab";
+
+  const imageUrl2 =
+    "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3";
   return (
     <>
       <Navbar />
       <Header />
-      
 
       <br />
       <div className="cards">
@@ -56,8 +52,8 @@ function App() {
           />
         </div>
       </div>
-     
-      <FooterJs/>
+
+      <FooterJs />
     </>
   );
 }
