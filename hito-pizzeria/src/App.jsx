227f5e64680ec4e1f8c4585b73fb11ba3ed7d1be
ROW <axlrower@gmail.com>
2025-04-19 13:21:27 -0400

    actualizacion de la estructura

--- hito-pizzeria/src/App.jsx
+++ hito-pizzeria/src/App.jsx
@@ -1,63 +1,19 @@
 import { useState } from "react";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-import CardPizza from "./components/Cards/CardPizza";
 import FooterJs from "./components/Footer/FooterJs";
 import FormularioRegistro from "./components/FormularioRegistro/FormularioRegistro";
 import FormularioLogin from "./components/FormularioLogin/FormularioLogin";
 
-
 function App() {
-  const imageUrl =
-    "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c";
-
-  const imageUrl1 =
-    "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab";
-
-  const imageUrl2 =
-    "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3";
   return (
     <>
-
       <Navbar />
-      <Home />
       <FormularioRegistro />
       {/* <FormularioLogin /> */}
+      <Home />
 
       <br />
-      <div className="cards">
-        <div className="card1">
-          <CardPizza
-            imageUrl={imageUrl}
-            name="Pizza Napolitana"
-            ingredients={["🍕 mozzarella,", "tomates,", "jamón,", "orégano"]}
-            price="Precio:$5.950"
-          />
-        </div>
-
-        <div className="card2">
-          <CardPizza
-            imageUrl={imageUrl1}
-            name="Pizza Española"
-            ingredients={[
-              "🍕 mozzarella,",
-              "gorgonzola,",
-              "parmesano,",
-              "provolone",
-            ]}
-            price="Precio:$6.950"
-          />
-        </div>
-
-        <div className="card3">
-          <CardPizza
-            imageUrl={imageUrl2}
-            name="Pizza Peperoni"
-            ingredients={["🍕 mozzarella,", "pepperoni", "orégano"]}
-            price="Precio:$6.950"
-          />
-        </div>
-      </div>
 
       <FooterJs />
     </>
--- hito-pizzeria/src/components/Home/Home.jsx
+++ hito-pizzeria/src/components/Home/Home.jsx
@@ -1,18 +1,54 @@
 import React from "react";
 import Header from "../Header/Header";
-
+import CardPizza from "../Cards/CardPizza";
 
 export default function Home() {
-  
+  const imageUrl =
+    "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c";
+
+  const imageUrl1 =
+    "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fcheese-164872_640_com.jpg?alt=media&token=18b2b821-4d0d-43f2-a1c6-8c57bc388fab";
+
+  const imageUrl2 =
+    "https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3";
 
   return (
     <div>
-         <Header />  
+      <Header />
 
+      <div className="cards">
+        <div className="card1">
+          <CardPizza
+            imageUrl={imageUrl}
+            name="Pizza Napolitana"
+            ingredients={["🍕 mozzarella,", "tomates,", "jamón,", "orégano"]}
+            price="Precio:$5.950"
+          />
+        </div>
 
+        <div className="card2">
+          <CardPizza
+            imageUrl={imageUrl1}
+            name="Pizza Española"
+            ingredients={[
+              "🍕 mozzarella,",
+              "gorgonzola,",
+              "parmesano,",
+              "provolone",
+            ]}
+            price="Precio:$6.950"
+          />
+        </div>
 
-
-         
+        <div className="card3">
+          <CardPizza
+            imageUrl={imageUrl2}
+            name="Pizza Peperoni"
+            ingredients={["🍕 mozzarella,", "pepperoni", "orégano"]}
+            price="Precio:$6.950"
+          />
+        </div>
+      </div>
     </div>
   );
 }
