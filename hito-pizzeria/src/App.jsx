78ab5fab4e8847e3c49385b08c9bda5dee7ada5c
Row <axlrower@gmail.com>
2025-05-10 16:02:01 -0400

    creacion del componente notfound

--- hito-pizzeria/src/App.jsx
+++ hito-pizzeria/src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from "react";
-import Home from "./components/views/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
-import FooterJs from "./components/Footer/FooterJs";
-// import Pizza from "./components/Pizzas/Pizza.jsx";
-// import Cart from "./components/Cart/Cart";
-//import FormularioRegistro from "./components/FormularioRegistro/FormularioRegistro";
-// import FormularioLogin from "./components/FormularioLogin/FormularioLogin";
 
+import Home from "./components/views/Home/Home.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import FooterJs from "./components/Footer/FooterJs.jsx";
+import { Routes, Route } from "react-router-dom";
+// import Pizza from "./components/Pizzas/Pizza.jsx";
+import Cart from "./components/views/Cart/Cart.jsx"
+import FormularioRegistro from "./components/views/FormularioRegistro/FormularioRegistro.jsx"
+import FormularioLogin from "./components/views/FormularioLogin/FormularioLogin.jsx"
+import NotFound from "./components/views/NotFound.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <FormularioRegistro /> */}
-      {/* <FormularioLogin /> */}
-      <Home />
-      {/* <Cart /> */}
-      {/* <Pizza /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<FormularioRegistro />} />
+        <Route path="/login" element={<FormularioLogin />} />
+        <Route path="/cart" element={<Cart />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
 
       <FooterJs />
     </>
