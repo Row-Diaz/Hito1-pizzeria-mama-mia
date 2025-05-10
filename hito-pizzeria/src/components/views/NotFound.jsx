09c084e401b16cff845e1e4a0faa6c05bb50f45a
Row <axlrower@gmail.com>
2025-05-10 17:13:39 -0400

    finalizando el componente notfound

--- hito-pizzeria/src/components/views/NotFound.jsx
+++ hito-pizzeria/src/components/views/NotFound.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
+import "./NotFound.css"
 
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Página no encontrada</h1>
-      <Link to="/">Volver a la página principal</Link>
+    <div >
+     
+      <img className='not-found-img' src="https://c8.alamy.com/comp/2BTM8W9/gloomy-face-of-slice-of-pizza-cartoon-character-with-404-boards-2BTM8W9.jpg" alt="" />
+      <Link to="/"><button className="not-found-btn">Home</button></Link>
     </div>
   );
 }
 
 export default NotFound;
+
